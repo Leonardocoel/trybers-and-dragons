cd9428730a6000b8d2f5742d00ab761e48b99fee
Leonardo C <leonardocoel98@gmail.com>
2022-08-09 19:08:26 -0300

    feat: create Fighter interface

---
+++ src/Fighter/Fighter.ts
@@ -0,0 +1,10 @@
+export default interface Fighter {
+  lifePoints: number;
+  strength: number;
+  defense: number;
+  energy?: number;
+  attack(enemy: Fighter): void;
+  special?(enemy: Fighter): void;
+  levelUp(): void;
+  receiveDamage(attackPoints: number): number;
+}
